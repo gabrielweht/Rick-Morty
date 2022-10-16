efae8b162ba9b428ae61c7dd47294ea308bd4340
gabrielweht <gabrielestebanw@gmail.com>
2022-10-15 21:55:46 -0300

    Add functions to see the characters searched

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { CharactersService } from 'src/app/core/services/characters.service';
+
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
+
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  names: any = []
+  responses: any = {}
+  value$: string = ''
+
+  constructor(private searchComponent: CharactersService) { }
 
   ngOnInit(): void {
+  }
+
+  search(event: any) {
+
+    this.value$ = event.target.value
+
+    this.searchComponent.searchCharacter(event.target.value.trim()).subscribe(results => {
+      if(event.target.value === '') this.names = []
+      else this.names = results.results.slice(0, 10).map( char => {
+        return {
+          name: char.name,
+          id: char.id
+        }
+      });
+      this.responses = results.results
+    })
+
+  }
+
+  goToCharacterId(){
+    this.names = []
+    this.value$ = ''
+  }
+
+  searchCharacters(){
+    this.searchComponent.characters$.emit(this.responses)
+    this.names = []
+    this.value$ = ''
   }
 
 }
