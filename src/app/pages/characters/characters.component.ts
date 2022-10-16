12b10342e4db18e69217b600f356e5ac79b99e8e
gabrielweht <gabrielestebanw@gmail.com>
2022-10-15 21:58:04 -0300

    Delete console.logs

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { CharactersService } from 'src/app/services/characters.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CharactersService } from 'src/app/core/services/characters.service';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.css']
 })
-export class CharactersComponent implements OnInit {
+export class CharactersComponent implements OnInit, OnDestroy{
 
   public characters: any = [];
 
   constructor( private characterService: CharactersService) { }
 
+
+
   ngOnInit(): void {
 
-    this.characterService.findCharacters()
+    if(this.characterService.characters$.length > 0) {
+      this.characterService.characters$.subscribe( response => this.characters = response)
+    }
+
+    else {
+      this.characterService.findCharacters()
       .subscribe( (resp) => {
-        console.log(resp.results);
-        this.characters = resp.results;
+        this.characterService.characters$.emit(resp.results);
       })
 
+      this.characterService.characters$.subscribe( response => this.characters = response)
+    }
+
+
+  }
+
+
+  //hay que hacer que se mantengan los personajes buscados
+  ngOnDestroy(): void {
+    this.characterService.characters$.emit(this.characters);
   }
 
 }
