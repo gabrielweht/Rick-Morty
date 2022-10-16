5c6f47cf540f57f2fb759dffc0d6cb814c922e40
gabrielweht <gabrielestebanw@gmail.com>
2022-10-15 21:54:03 -0300

    Add imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+
 import { AppComponent } from './app.component';
 import { CharactersComponent } from './pages/characters/characters.component';
 import { CharacterComponent } from './pages/character/character.component';
