5f90f7189846671a3ab6584be498ad08662d55c7
gabrielweht <gabrielestebanw@gmail.com>
2022-11-15 21:14:40 -0300

    Fix css to mobile

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  @ViewChild('asMenu') menu!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer2: Renderer2) { }
 
   ngOnInit(): void {
+  }
+
+  activeMenu(){
+    const asMenu = this.menu.nativeElement
+    this.renderer2.addClass(asMenu, "menuActive")
+    this.renderer2.removeClass(asMenu, "elementsRight")
   }
 
 }
